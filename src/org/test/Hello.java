ba99c1e969d87a8ecbef3cbae5a52f5a8f2cdf4f
shivamani455 <shivamani455@gmail.com>
2021-02-25 09:56:30 +0530

    my test 2

---
+++ src/org/test/Hello.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Hello {
+	public static void main(String[] args) {
+	  
+	    String s1="WelcomE";
+	  String l1= s1.toUpperCase();
+	 
+	   System.out.println(l1);
+	    
+	}
+	}
+
+
